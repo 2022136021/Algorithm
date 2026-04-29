1422fa4a7a39a6c2e55a398b012b6dd57e4b6eb7
2022136021 <2022136021@g.shingu.ac.kr>
2026-04-30 00:32:27 +0900

    Add files via upload

---
+++ ch05stackEx/Ch5StackEx/ex5_1.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <windows.h>
+#include "stackS.h"
+
+int main(void) 
+{
+	SetConsoleOutputCP(65001);//UTF-8,CP는 Code Page의 약자
+	SetConsoleCP(65001); // 입력도 필요하면 설정
+
+	element item;
+	printf("\n** 순차 스택 연산 **\n");
+	printStack();
+	push(1);	printStack();		// 1 삽입
+	push(2);  printStack();		// 2 삽입
+	push(3);	printStack();		// 3 삽입
+
+	item = peek();  printStack();	// 현재 top의 원소 출력
+	printf("peek => %d", item);
+
+	item = pop();  printStack();	// 삭제
+	printf("\t pop  => %d", item);
+
+	item = pop();  printStack();	// 삭제
+	printf("\t pop  => %d", item);
+
+	item = pop();  printStack();	// 삭제
+	printf("\t pop  => %d\n", item);
+
+	//getchar();  
+	return 0;
+}
---
+++ ch05stackEx/Ch5_2_StackByLinkedList/ex5_2.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <windows.h>
+#include "stackL.h"
+
+int main(void) 
+{
+	SetConsoleOutputCP(65001); //UTF-8,CP는 Code Page의 약자
+	SetConsoleCP(65001); // 입력도 필요하면 설정
+
+	element item = 0 ;
+	top = NULL;
+	printf("\n** 연결 스택 연산 **\n");
+	printStack();
+
+	push(1);    printStack();		// 1 삽입
+	push(2);    printStack();		// 2 삽입
+	push(3);	  printStack();		// 3 삽입
+
+	item = peek(); printStack();	// 현재 top의 원소 출력
+	printf("peek => %d", item);
+
+	item = pop();  printStack();	// 삭제
+	printf("\t pop  => %d", item);
+
+	item = pop();  printStack();	// 삭제
+	printf("\t pop  => %d", item);
+
+	item = pop();  printStack();	// 삭제
+	printf("\t pop  => %d\n", item);
+
+	//getchar();  
+
+	return 0;
+}
