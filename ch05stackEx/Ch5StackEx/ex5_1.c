f84c0fd99c2b5ad1fe39d3b7144a03d414f23abb
2022136021 <2022136021@g.shingu.ac.kr>
2026-05-03 23:15:50 +0900

    Update ex5_1.c

--- ch05stackEx/Ch5StackEx/ex5_1.c
+++ ch05stackEx/Ch5StackEx/ex5_1.c
@@ -4,14 +4,14 @@
 
 int main(void) 
 {
-	SetConsoleOutputCP(65001);//UTF-8,CP는 Code Page의 약자
-	SetConsoleCP(65001); // 입력도 필요하면 설정
+	SetConsoleOutputCP(65001);	//UTF-8,CP는 Code Page의 약자
+	SetConsoleCP(65001); 		// 입력도 필요하면 설정
 
 	element item;
 	printf("\n** 순차 스택 연산 **\n");
 	printStack();
 	push(1);	printStack();		// 1 삽입
-	push(2);  printStack();		// 2 삽입
+	push(2);  printStack();			// 2 삽입
 	push(3);	printStack();		// 3 삽입
 
 	item = peek();  printStack();	// 현재 top의 원소 출력
